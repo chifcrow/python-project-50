9c919b1f3886cd585cdd5a0af3944dc4a1e7aeca
lazer <chifcrow@users.noreply.github.com>
2024-04-13 09:51:45 +0300

    Hexlet-code packager

---
+++ hexlet_code/scripts/gendiff.py
@@ -0,0 +1,14 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate diff")
+    parser.add_argument('first_file', help="first file to compare")
+    parser.add_argument('second_file', help="second file to compare")
+    parser.add_argument('-f', '--format', help="set format of output", default='stylish')
+
+    args = parser.parse_args()
+    print(f"Comparing {args.first_file} and {args.second_file}, format: {args.format}")
+
+if __name__ == "__main__":
+    main()
+    
