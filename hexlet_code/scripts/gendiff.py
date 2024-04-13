1a424bb81da0346268a48b4591fd304dc6695feb
chifcrow <office@devijen.com>
2024-04-13 13:15:44 +0300

    gendiff.py lint editing

--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -5,10 +5,13 @@ def main():
     parser = argparse.ArgumentParser(description="Generate diff")
     parser.add_argument('first_file', help="first file to compare")
     parser.add_argument('second_file', help="second file to compare")
-    parser.add_argument('-f', '--format', help="set format of output", default='stylish')
+    parser.add_argument('-f', '--format', help="set format of output",
+                        default='stylish')
 
     args = parser.parse_args()
-    print(f"Comparing {args.first_file} and {args.second_file}, format: {args.format}")
+    print(f"Comparing {args.first_file} and {args.second_file},"
+          f" format: {args.format}")
+
 
 if __name__ == "__main__":
     main()
